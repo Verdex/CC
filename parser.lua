4449c1a6b1217b7b6e53469d6eda9458ae7c7b40
David L. Inman <davidlevere@gmail.com>
2017-06-24 23:28:43 -0400

    pattern parser

--- parser.lua
+++ parser.lua
@@ -3,6 +3,7 @@ require "token"
 
 local _tokens
 local _index 
+local _result
 
 function init( tokens )
     _tokens = tokens
@@ -18,7 +19,8 @@ function nt()
 end
 
 function try( tok )
-    if tok == ct() then
+    local c = ct()
+    if c and tok == c.type then
         nt()
         return true
     else 
@@ -27,11 +29,45 @@ function try( tok )
 end
 
 function is( tok )
-    if tok == ct() then
+    local c = ct()
+    if c and tok == c.type then
         nt()
+        return true
     else
-        error( "Expected:  " .. tok .. ", but found:  " .. ct() )
+        if c ~= nil then
+            error( "Expected:  " .. tok .. ", but found:  " .. c.type .. " at: " .. _index )
+        else
+            error( "Current token is nil at: " .. _index )
+        end
     end
 end
 
+--[[
+    blah = other { expr '+' expr }
+         | jab   { expr '-' expr }
 
+{
+    type = blah
+    kind = other
+    1 = expr1
+    2 = expr2 
+]]--
+
+function pattern()
+    local finished = false
+    local ret = {}
+
+    is( tokenType.openCurly )
+    repeat
+        local c = ct()
+        if try( tokenType.symbol ) then
+            ret[#ret+1] = c
+        elseif try( tokenType.string ) then
+
+        elseif is( tokenType.closeCurly ) then
+            finished = true
+        end
+    until finished
+
+    return ret
+end
