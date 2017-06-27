50194744f073e150fc27f62c1ca985ac5350b1ac
David L. Inman <davidlevere@gmail.com>
2017-06-26 22:59:37 -0400

    cc.lua

---
+++ cc.lua
@@ -0,0 +1,18 @@
+
+require 'lexer'
+require 'parser'
+require 'util'
+
+
+args = {...}
+
+if not args[1] then
+    error( "usage:  cc file.cc" )
+end
+
+f = io.open( args[1], "r" )
+s = f:read("a")
+l = lex( s )
+p = parse( l )
+
+print( d( p ) )
